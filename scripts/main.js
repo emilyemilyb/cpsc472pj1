bae3bb93cb1b42b48d7dfbff4063b1db3f7ed2dc
snehalsj13 <snehal.snehal4@gmail.com>
2018-03-31 18:16:30 -0400

    +main, +index

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,17 @@
+(function(window) {
   "use strict"
   var DETAIL_IMAGE_SELECTOR = "[data-image-role='displayImage']";
   var THUMBNAIL_LINK_SELECTOR = "[data-image-role='trigger']";
-<<<<<<< HEAD
+  //<<<<<<< HEAD
+  var App = window.App;
   var SERVER_URL = "http://localhost:2403/";
   var RemoteDataStore = App.RemoteDataStore;
   var remoteDS = new RemoteDataStore(SERVER_URL);
-=======
+  //=======
   var SERVER_URL = "http://localhost:2404/coffeeorders";
   //var RemoteDataStore = App.RemoteDataStore;
-//  var remoteDS = new RemoteDataStore(SERVER_URL);
->>>>>>> ca4e65b239f409cf54b4e0636239e97575aa05ce
+  //  var remoteDS = new RemoteDataStore(SERVER_URL);
+  //>>>>>>> ca4e65b239f409cf54b4e0636239e97575aa05ce
   var imageArray = [];
 
   /*  remoteDS.getAll(function(data) {
@@ -19,13 +21,13 @@
       }
     });
 */
-function setDetails(imageUrl) {
-  "use strict";
-  var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
-  detailImage.setAttribute("src", imageUrl);
-}
+  function setDetails(imageUrl) {
+    "use strict";
+    var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    detailImage.setAttribute("src", imageUrl);
+  }
 
-//  function imageFromThumb(thumbnail) {
+  //  function imageFromThumb(thumbnail) {
   //  return thumbnail.getAttribute("type");
   //}
 
@@ -38,11 +40,11 @@ function setDetails(imageUrl) {
     //var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
     //var thumbnailArray = [].slice.call(thumbnails);
     //return thumbnailArray;
-    imageArray[0]="img/Spaghetti.jpg";
-    imageArray[1]="img/Taco.jpg";
-    imageArray[2]="img/Breakfast.jpg";
-    imageArray[3]="img/Hamburger.jpg";
-    imageArray[4]="img/Ramen.jpg";
+    imageArray[0] = "img/Spaghetti.jpg";
+    imageArray[1] = "img/Taco.jpg";
+    imageArray[2] = "img/Breakfast.jpg";
+    imageArray[3] = "img/Hamburger.jpg";
+    imageArray[4] = "img/Ramen.jpg";
 
     return imageArray;
   }
@@ -73,3 +75,4 @@ function setDetails(imageUrl) {
     //setDetails(imageFromThumb(imageArray[prevImage]));
     setDetails(imageArray[prevImage]);
   }
+})(window);
