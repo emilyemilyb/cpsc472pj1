43f3ede5793565af1f700a0a0cad62daf3d3dd0b
snehalsj13 <snehal.snehal4@gmail.com>
2018-04-01 20:28:24 -0700

    +main

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@
   var FORM_SELECTOR_IMAGES = "[data-payment='form']";
   var SERVER_URL = "http://localhost:2404/foodwars";
   var RemoteDataStore = App.RemoteDataStore;
-  var remoteDS = new RemoteDataStore(SERVER_URL);  
+  var remoteDS = new RemoteDataStore(SERVER_URL);
   var THUMBNAIL_LINK_SELECTOR = "[data-image-role='trigger']";
   var FORM_SELECTOR_FOODWAR = "[data-foodwar='form']";
   var FormHandler = App.FormHandler;
@@ -40,30 +40,7 @@ imageArray[1]="img/Taco.jpg";
 imageArray[2]="img/Breakfast.jpg";
 imageArray[3]="img/Hamburger.jpg";
 imageArray[4]="img/Ramen.jpg";
-  formHandler.addPreviousHandler(getCurrentImageIndex(), imageArray);
-  formHandler.addNextHandler(getCurrentImageIndex(), imageArray);
 
 
-  function getCurrentImageIndex() {
-    //imageArray = getimageArray();
-    for (var i = 0; i < imageArray.length; i++) {
-      if (imageArray[i].text == detailImage.getAttribute("src")) {
-        return i;
-      }
-    }
-    return -1;
-  }
-
-  function next() {
-    imageArray = getThumbnailsArray();
-    var nextImage = (getCurrentImageIndex() + 1) % thumbnailArray.length;
-    setDetails(imageFromThumb(imageArray[nextImage]));
-  }
-
-  function previous() {
-    imageArray = getThumbnailsArray();
-    var prevImage = (getCurrentImageIndex() - 1 + thumbnailArray.length) % thumbnailArray.length;
-    setDetails(imageFromThumb(imageArray[prevImage]));
-  }
-
+  
 })(window);
